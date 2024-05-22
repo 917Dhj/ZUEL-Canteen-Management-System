06e515da0d198aa46f29cd5ffee20320f76ee917
dingdingcar <135248415+917Dhj@users.noreply.github.com>
2024-05-23 01:40:23 +0800

    Add files via upload

---
+++ Java后端源代码/MiniProgramManagement/src/test/java/com/example/miniprogrammanagement/MiniProgramManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprogrammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProgramManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
